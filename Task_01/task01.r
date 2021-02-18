1cfcd3d2e98ddde52088e1b96c2b54381542c830
Bwingrove99 <baw0021@mix.wvu.edu>
2021-02-18 16:28:00 -0500

    Blake Wingrove Task 04

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -73,3 +73,70 @@ unique(beren3$event)
 
 Question 1: There is not enough data
 question 2: The x axis is too small of a scale and the data is too clumbed to read.
+
+trueMean1 <- 5
+trueSD1 <- 5
+population1 <- rnorm(1e6, trueMean1, trueSD1)
+trueMean2 <- 4
+trueSD2 <- 5
+population2 <- rnorm(1e6, trueMean2, trueSD2)
+Size <-50
+Sample1 <- sample(population1 , Size)
+Sample2 <- sample(population2 , Size)
+# yes they are different and so were the populations 
+boxplot(Sample1, Sample2)
+source("http://jonsmitchell.com/code/simFxn04.R")
+MatGrandma <- makeFounder("grandma_mom")
+MatGrandpa <- makeFounder("grandpa_mom")
+PatGrandma <- makeFounder("grandma_da")
+PatGrandpa <- makeFounder("grandpa_da")
+Alan <- makeBaby(PatGrandma, PatGrandpa)
+Brenda <- makeBaby(MatGrandma, MatGrandpa)
+Focus <- makeBaby(Brenda, Alan)
+# Should be 50% or .5
+ToMom <- length( grep("mom", Focus))/ length(Focus)
+# 0.3076 and .1924. Doesn’t match my prediction
+ToMomMom<- length( grep( "grandma_mom", Focus))/ length(Focus)
+ToMomDad <- length( grep( "granpa_mom", Focus))/ length(Focus)
+# Focus inst equal with relation to the maternal grandparents and the paternal grandparents. Average relatedness was 0.25. 
+Sibling_01<-makeBaby(Brenda, Alan)
+# I would expect 50%, a ctual was 47%.
+ToSib <- length(intersect( Focus, Sibling_01))/ length(Focus)
+ManySiblings <- replicate( 1e3, length( intersect( Focus, makeBaby(Brenda, Alan)))/length( Focus))
+#Shares different numbers of genes with each 1000 siblings.
+quantile(ManySiblings)
+mean(ManySiblings)
+plot(density(ManySiblings), main="", xlab="proportion shared genes")
+HWE <- function(p)  {
+	aa<-p^2
+	ab<-2*p*(1-p)
+	bb<-(1-p)^2
+	return(c(aa=aa, ab=ab, bb=bb))
+	}
+HWE(0.5)
+plot(1, 1, type="n", xlim=c(0,1), xlab="freq. allele a", ylab="geno. freq")
+p<- seq(from = 0, to = 1, by = 0.01)
+GenoFreq<- t(sapply(p, HWE))
+lines(p, GenoFreq[,"aa"], lwd=2, col="red")
+# Frequency of aa increases as the frequency of alleles a aincrease in the population. As it decreases, frequency does too. Time is not shown in the plot. Geographical space is not as well.
+lines(p, GenoFreq[,"ab"], lwd=2, col="purple")
+lines(p, GenoFreq[,"bb"], lwd=2, col="blue")
+legend("top", legend=c("aa","ab","bb"), col=c("red","purple","blue"), lty=1, lwd=2, bty="n")
+Pop<- simPop(500)
+points(Pop[,"freqa"], Pop[,"Genotypes.aa"]/500, pch=21, bg="red")
+#Does not match the Hardy Weinberg expectation
+Pop<-simPop(50)
+points(Pop[,"freqa"], Pop[,"Genotypes.aa"]/50, pch=22, bg="red")
+# More points on the graph. 
+install.packages("learnPopGen")
+library(learnPopGen)
+x<-genetic.drift(Ne=200, nrep=5, pause=0.01)
+PopSizes<-5:50
+Samples<- rep(PopSizes, 5)
+tExt<- sapply(Samples, function(x) nrow(simPop(x, 500)))
+Line<- lm(tExt ~ Samples)
+summary(Line)
+Line$coef
+plot(Samples, tExt)
+abline(Line)
+# as population increases, the distance from the line increases. Population size increases, there is more extinction for alleles.
