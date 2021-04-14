6c9964cbc02d0b6646270ebd960f101ea4cbe1ac
Bwingrove99 <77845926+Bwingrove99@users.noreply.github.com>
2021-04-14 12:49:16 -0400

    Task 09

---
+++ task09.r
@@ -0,0 +1,29 @@
+setwd('~/Desktop/Evolution/tasks/Task_09')
+1
+1
+9
+Sys.Date()
+mean(c(2, 4, 5))
+submit()
+boring_function('My first function!')
+boring_function
+submit()
+my_mean(c(4, 5, 10))
+submit()
+remainder(5)
+remainder(11, 5)
+remainder(divisor = 11, num = 5)
+remainder(4, div = 2)
+args(remainder)
+submit()
+evaluate(sd, c(1.4, 3.6, 7.9, 8.8))
+evaluate(function(x){x+1}, 6)
+evaluate(function(x){x[1]}, c(8, 4, 0))
+evaluate(function(x){x[length(x)]}, c(8, 4, 0))
+?paste
+paste("Programming", "is", "fun!")
+submit()
+submit()
+mad_libs(adjective = "", place = "", noun = "")
+submit()
+'I' %p% 'love' %p% 'R!'
